cb6ff7add072c3e20abf4e5452e2ce27edf84f9d
BALAJI S <balajisubash956@gmail.com>
2025-06-25 11:35:09 +0530

    Change made in server,js

--- server.js
+++ server.js
@@ -48,11 +48,11 @@ app.use((req, res, next) => {
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-if (process.env.NODE_ENV === 'test') {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 
 
 export default app;
