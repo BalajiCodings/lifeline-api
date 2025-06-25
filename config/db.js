3eba568bdfb578ea521e000c73a1dec64a44d1b9
BALAJI S <balajisubash956@gmail.com>
2025-06-25 11:11:18 +0530

    Update changes in db file

--- config/db.js
+++ config/db.js
@@ -2,18 +2,12 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        const mongoURI = process.env.NODE_ENV === 'test'
-        ? process.env.MONGO_URI_TEST
-        : process.env.MONGO_URI;
-        await mongoose.connect(mongoURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("Connected to balaji's database");
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to database");
     } catch (error) {
-        console.error(" Database connection error:", error);
+        console.error("Database connection error:", error);
         process.exit(1);
     }
-}
+};
 
 export default connectDB;
